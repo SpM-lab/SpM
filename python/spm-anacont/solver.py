c44e3662f2d1e049964394178e6e4c533de194a3
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-10-19 23:34:03 +0900

    minor updates

--- python/spm-anacont/solver.py
+++ python/spm-anacont/solver.py
@@ -8,7 +8,7 @@ default_spm_input_params = {
    # INPUT/OUTPUT
    #"statistics": "fermion",
    #"beta": 4,
-   "filein_G": "gtau.txt",
+   "filein_G": "gtau.in",
    "column": 1,
    "fileout_spec": "spectrum.dat",
    # OMEG,
@@ -36,6 +36,7 @@ class SpMSolver:
     def __init__(self, exec_path='SpM.out', work_dir='work_SpM', **kwargs):
         self.exec_path = exec_path
         self.work_dir = work_dir
+        self.stdout_err = None
 
         additional_params = copy.deepcopy(valid_kwargs)
         for k, v in kwargs:
@@ -50,13 +51,25 @@ class SpMSolver:
             self.spm_input_params[k] = v
 
 
-    def solve(self):
+    def solve(self, gtau):
+        """
+        Perform analytic continuation
+
+        gtau: 1D array of float
+            Green's function to be continued
+        return: 1D array of float
+            Spectral function
+        """
+        gtau = np.asarray(gtau)
+        assert not np.iscomplexobj(gtau)
+        assert gtau.ndim == 1
+
         dir_org = os.getcwd()
         if os.path.exists(self.work_dir):
             if os.path.isdir(self.work_dir):
                 os.removedirs(self.work_dir)
             else:
-                raise RuntimeError("Path {self.work_dir} exits but it's not a directory!")
+                raise RuntimeError("Path {self.work_dir} is not a directory!")
 
         try:
             os.chdir(self.work_dir)
@@ -65,18 +78,23 @@ class SpMSolver:
                 for k, v in self.spm_input_params:
                     print(f'{k} = {v}', file=f)
 
+            xs = np.linspace(0, 1, gtau.size)
+            np.savetxt('gtau.in', np.vstack((xs, gtau)).T)
+
             commands = [self.exec_path, 'param.in']
-            return_code = subprocess.call(commands, stdout='stdout.txt', stderr=subprocess.STDOUT)
+            return_code = subprocess.call(commands, stdout='output.txt', stderr=subprocess.STDOUT)
+            with open('output.txt') as f:
+                self.stdout_err = f.readlines()
 
             if return_code:
                 raise RuntimeError("Subprocess of SpM returned error!")
             
             res = np.loadtxt('spectrum.dat')
-            # Load additional results
+            # TODO: Load additional results
 
             os.chdir(dir_org)
 
-            return res[:,1] + 1j * res[:,2]
+            return res[:,1]
 
         except Exception as e:
             os.chdir(dir_org)
